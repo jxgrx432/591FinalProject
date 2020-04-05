db182c9acc48763d61cef18dfa79b6f292214ddd
John Griffin <63033839+jxgrx432@users.noreply.github.com>
2020-04-05 09:55:53 -0700

    Update UserInstructions.java

--- SpotifyProject/src/UserInstructions.java
+++ SpotifyProject/src/UserInstructions.java
@@ -1,5 +1,19 @@
-package application;
+/**
+ * Class to hold user instructions and related JavaFX graphic elements to pass to MainDisplay
+ * @author johngriffin
+ *
+ */
 
 public class UserInstructions {
 
+	// instance variables
+	String userInstructionText = "This application generates a playlist of songs based on user input. Please select the relevant input parameters and when ready press the 'Generate Playlist' button.";
+	
+	// constructor
+	public UserInstructions() {
+	}
+	
+	// methods
+	
+	
 }

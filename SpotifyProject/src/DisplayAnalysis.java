6a4b9fa2d63c16303a7dd77ace183f319a1e4834
John Griffin <63033839+jxgrx432@users.noreply.github.com>
2020-04-05 09:53:38 -0700

    Update DisplayAnalysis.java

--- SpotifyProject/src/DisplayAnalysis.java
+++ SpotifyProject/src/DisplayAnalysis.java
@@ -1,5 +1,13 @@
-package application;
+// This class is used to display analysis of the data to the user
 
 public class DisplayAnalysis {
 
+	/**
+	 * Creates a class constructor for the DisplayAnalysis class
+	 */
+	public DisplayAnalysis() {
+		
+	}
+	
+	
 }
